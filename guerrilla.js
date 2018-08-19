0db16933e6af2ebedfc12a07fe1274c2a890465a
xiaxinti <michelle.xia@mail.utoronto.ca>
2018-08-18 22:20:18 -0400

    added span around guerrilla group name

--- guerrilla.js
+++ guerrilla.js
@@ -1,236 +1,227 @@
-	function fmtDate(d) {
-		var s = d.getMonth() + '/' + d.getDate() + ' ' + ("0" + d.getHours()).slice(-2) + ':' + ("0" + d.getMinutes()).slice(-2);
-		return s;
-	}
-	
-	function refreshSetting() {
-		if(window.localStorage.getItem('region') === 'NA'){
-			$('.NA').css('display', 'block');
-			$('.JP').css('display', 'none');
-		}else{
-			$('.NA').css('display', 'none');
-			$('.JP').css('display', 'block');
-		}
-		var modes = ['.group', '.schedule', '.next'];
-		for (var m of modes){
-			$(m).css('display', 'none');
-		}
-		$('.'+window.localStorage.getItem('mode')).css('display', 'block');
-	}
-	function switchRegion(){
-		if(window.localStorage.getItem('region') === 'NA'){
-			window.localStorage.setItem('region', 'JP');
-		}else{
-			window.localStorage.setItem('region', 'NA');
-		}
-		refreshSetting();
-	}
-	function pickMode(mode){
-		window.localStorage.setItem('mode', mode);
-		refreshSetting();
-	}	
-	window.onload=function(){
-		function addRow(id, row) {
-			$(id+' > tbody:last-child').append(row);
-		};
-		function tr(text) {
-			return '<tr>' + text + '</tr>';
-		}
-		function td(text) {
-			return '<td>' + text + '</td>';
-		}
-		var icon = {};
-		function getIcon(dungeonName) {
-			var w = "50";
-			if(icon[dungeonName] != null){
-				var dungeonLink = (icon[dungeonName]["d"].length > 0) ? icon['dungeonURL'] + icon[dungeonName]["d"] : "#";
-				return "<a href=\"" + dungeonLink + "\" title=\""+dungeonName+"\"><img src=\"" + icon['iconURL'] + icon[dungeonName]["i"] + "\" width="+w+" height="+w+"/></a>";
-			}else{
-				return "<img src=\""+icon['iconURL']+"4014.png\" title=\""+dungeonName+"\" width="+w+" height="+w+"/>";
-			}
-		}
-		var timeGrouped = {'NA': new Map(), 'JP': new Map()};
-		var nextTime = {'NA': 0, 'JP': 0};
-		var activeTime = {'NA': 0, 'JP': 0};
-		function loadSortedData(data) {
-			var items = data['items'];
-			
-			var namedItems = {};
-			for (var x of items) {
-				var name = x['dungeon_name'];
-				var group = x['group'];
-				if (!(name in namedItems)) {
-					namedItems[name] = new Map();
-				}
-				if (!(namedItems[name].has(group))) {
-					namedItems[name].set(group, []);
-				}
-				namedItems[name].get(group).push(x);
-			}
-			for (var name in namedItems) {
-				var groupedItems = namedItems[name];
-				var firstItem = groupedItems.values().next().value[0];
-				var server = firstItem['server'];
-				var row = td(getIcon(name));
-				for (var g of ['A', 'B', 'C', 'D', 'E']) {
-					if (!(groupedItems.has(g))) {
-						continue;
-					}
-					row += '<td>';
-					for(var item of groupedItems.get(g)){
-						var now = new Date().getTime() ;
-						if(now > item['start_timestamp'] * 1000 && now < item['end_timestamp'] * 1000){
-							row += '<span class=\"highlight\">' + fmtDate(new Date(item['start_timestamp'] * 1000)) + '</span></br>';
-						}else{
-							row += '<span>' + fmtDate(new Date(item['start_timestamp'] * 1000)) + '</span></br>';
-						}
-					}
-					row = row.substring(0,row.length - 5) + '</td>';
-					
-					/*if (item && 'start_timestamp' in item) {
-						var now = new Date().getTime() ;
-						if(now > item['start_timestamp'] * 1000 && now < item['end_timestamp'] * 1000){
-							row += '<td class=\"highlight\">' + fmtDate(new Date(item['start_timestamp'] * 1000)) + '</td>';
-						}else{
-							row += td(fmtDate(new Date(item['start_timestamp'] * 1000)));
-						}
-					} else {
-						row += td('');
-					}*/
-				}
-				console.log(row);
-				addRow('#group'+server,tr(row));
-			}
-			
-			for (var i of items) {
-				var sts = i['start_timestamp'];
-				if(!timeGrouped[i['server']].has(sts)){
-					timeGrouped[i['server']].set(sts, []);
-				}
-				timeGrouped[i['server']].get(sts).push(i);
-			}
+function fmtDate(d) {
+	var s = d.getMonth() + '/' + d.getDate() + ' ' + ("0" + d.getHours()).slice(-2) + ':' + ("0" + d.getMinutes()).slice(-2);
+	return s;
+}
 
-			for (var server in timeGrouped){
-				timeGrouped[server] = new Map([...timeGrouped[server].entries()].sort());
-				timeGrouped[server].forEach(function(value, key, map) {
-					var row = td(fmtDate(new Date(key * 1000)));
-					value.sort(function(a,b){
-						return a['group'].toString().localeCompare(b['group'].toString());
-					});
-					
-					/*for (var dungeon of value){
-						row += td(dungeon['group'] + ' : ' +getIcon(dungeon['dungeon_name']));
-					}*/
-					
-					var g = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4};
-					var current = 0;
-					for (var i of value){
-						while(current < g[i['group']]){
-							row += td('');
-							current += 1;
-						}
-						var icon = getIcon(i['dungeon_name']);
-						if(current > g[i['group']]){
-							row = row.substring(0,row.length - 5) + '</br>' + icon + '</td>';
-						}else{
-							row += td(icon);
-							current += 1;
-						}
-					}
-					
-					var now = new Date().getTime()
-					if(now > key * 1000 && now < value[0]['end_timestamp'] * 1000){
-						row = '<tr class=\"highlight\">' + row + '</tr>';
+function refreshSetting() {
+	$('#region').html(window.localStorage.getItem('region'));
+	if(window.localStorage.getItem('region') === 'NA'){
+		$('.NA').css('display', 'block');
+		$('.JP').css('display', 'none');
+	}else{
+		$('.NA').css('display', 'none');
+		$('.JP').css('display', 'block');
+	}
+	var modes = ['.group', '.schedule', '.next'];
+	for (var m of modes){
+		$(m).css('display', 'none');
+	}
+	$('.'+window.localStorage.getItem('mode')).css('display', 'block');
+}
+function switchRegion(){
+	if(window.localStorage.getItem('region') === 'NA'){
+		window.localStorage.setItem('region', 'JP');
+	}else{
+		window.localStorage.setItem('region', 'NA');
+	}
+	refreshSetting();
+}
+function pickMode(mode){
+	window.localStorage.setItem('mode', mode);
+	refreshSetting();
+}	
+window.onload=function(){
+	function addRow(id, row) {
+		$(id+' > tbody:last-child').append(row);
+	};
+	function tr(text) {
+		return '<tr>' + text + '</tr>';
+	}
+	function td(text) {
+		return '<td>' + text + '</td>';
+	}
+	var icon = {};
+	function getIcon(dungeonName) {
+		var w = "50";
+		if(icon[dungeonName] != null){
+			var dungeonLink = (icon[dungeonName]["d"].length > 0) ? icon['dungeonURL'] + icon[dungeonName]["d"] : "#";
+			return "<a href=\"" + dungeonLink + "\" title=\""+dungeonName+"\"><img src=\"" + icon['iconURL'] + icon[dungeonName]["i"] + "\" width="+w+" height="+w+"/></a>";
+		}else{
+			return "<img src=\""+icon['iconURL']+"4014.png\" title=\""+dungeonName+"\" width="+w+" height="+w+"/>";
+		}
+	}
+	var timeGrouped = {'NA': new Map(), 'JP': new Map()};
+	var nextTime = {'NA': 0, 'JP': 0};
+	var activeTime = {'NA': 0, 'JP': 0};
+	function loadSortedData(data) {
+		var items = data['items'];
+		
+		var namedItems = {};
+		for (var x of items) {
+			var name = x['dungeon_name'];
+			var group = x['group'];
+			if (!(name in namedItems)) {
+				namedItems[name] = new Map();
+			}
+			if (!(namedItems[name].has(group))) {
+				namedItems[name].set(group, []);
+			}
+			namedItems[name].get(group).push(x);
+		}
+		for (var name in namedItems) {
+			var groupedItems = namedItems[name];
+			var firstItem = groupedItems.values().next().value[0];
+			var server = firstItem['server'];
+			var row = td(getIcon(name));
+			for (var g of ['A', 'B', 'C', 'D', 'E']) {
+				if (!(groupedItems.has(g))) {
+					continue;
+				}
+				row += '<td>';
+				for(var item of groupedItems.get(g)){
+					var now = new Date().getTime() ;
+					if(now > item['start_timestamp'] * 1000 && now < item['end_timestamp'] * 1000){
+						row += '<span class=\"highlight\">' + fmtDate(new Date(item['start_timestamp'] * 1000)) + '</span></br>';
 					}else{
-						row = tr(row);
+						row += '<span>' + fmtDate(new Date(item['start_timestamp'] * 1000)) + '</span></br>';
 					}
+				}
+				row = row.substring(0,row.length - 5) + '</td>';
+			}
+			console.log(row);
+			addRow('#group'+server,tr(row));
+		}
+		
+		for (var i of items) {
+			var sts = i['start_timestamp'];
+			if(!timeGrouped[i['server']].has(sts)){
+				timeGrouped[i['server']].set(sts, []);
+			}
+			timeGrouped[i['server']].get(sts).push(i);
+		}
 
-					addRow('#schedule'+server,row);
+		for (var server in timeGrouped){
+			timeGrouped[server] = new Map([...timeGrouped[server].entries()].sort());
+			timeGrouped[server].forEach(function(value, key, map) {
+				var row = td(fmtDate(new Date(key * 1000)));
+				value.sort(function(a,b){
+					return a['group'].toString().localeCompare(b['group'].toString());
 				});
-				nextTime[server] = timeGrouped[server].values().next().value[0]['start_timestamp'];
-			}
-		}
-		function populate() {
-			$.getJSON('./guerrilla_icon.json').done(function(data){
-				icon = data;
-			});
-			$.getJSON('https://storage.googleapis.com/mirubot/paddata/merged/guerrilla_data.json').done(loadSortedData);
-			//$.getJSON('./guerrilla_data.json').done(loadSortedData);
-		}
-		function cdUpdate() {
-			var now = new Date().getTime();
-			for (var server in timeGrouped){
-				if(nextTime[server] * 1000 - now < 0){
-					var next = 0;
-					for (let [k, v] of timeGrouped[server]) {
-						if(k * 1000 > now){
-							next = k;
-							break;
-						}
-					}
-					nextTime[server] = next;
-				}
-				if(activeTime[server] * 1000 - now < 0){
-					var next = 0;
-					for (let [k, v] of timeGrouped[server]) {
-						if(k * 1000 < now && v[0]['end_timestamp'] * 1000 > now){
-							next = k;
-							break;
-						}
-					}
-					activeTime[server] = next;
-				}
-				var clock;
-				var icon;
-				if(nextTime[server] > 0){
-					var distance = nextTime[server] * 1000 - now;
-					var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-					var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-					clock = hours + "h " + minutes + "m ";
-					icon = '';
-					for (var dungeon of timeGrouped[server].get(nextTime[server])){
-						icon += dungeon['group'] + ' : ' +getIcon(dungeon['dungeon_name']) + '</td><td>';
-					}
-					icon.substring(0, icon.length-4);
-				}else{
-					clock = 0;
-					icon = "None";
-				}
-				$('#nextTime'+server).html(clock);
-				$('#nextDungeon'+server).html(icon);
 				
-				if(activeTime[server] > 0){
-					var endtime = timeGrouped[server].get(activeTime[server])[0]['end_timestamp'];
-					var distance = endtime * 1000 - now;
-					var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-					var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-					clock = hours + "h " + minutes + "m";
-					icon = '';
-					for (var dungeon of timeGrouped[server].get(activeTime[server])){
-						icon += dungeon['group'] + ' : ' +getIcon(dungeon['dungeon_name']);
+				/*for (var dungeon of value){
+					row += td(dungeon['group'] + ' : ' +getIcon(dungeon['dungeon_name']));
+				}*/
+				
+				var g = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4};
+				var current = 0;
+				for (var i of value){
+					while(current < g[i['group']]){
+						row += td('');
+						current += 1;
 					}
-				}else{
-					clock = "--h --m";;
-					icon = "None";
+					var icon = getIcon(i['dungeon_name']);
+					if(current > g[i['group']]){
+						row = row.substring(0,row.length - 5) + '</br>' + icon + '</td>';
+					}else{
+						row += td(icon);
+						current += 1;
+					}
 				}
-				$('#activeTime'+server).html(clock);
-				$('#activeDungeon'+server).html(icon);
-			}
+				
+				var now = new Date().getTime()
+				if(now > key * 1000 && now < value[0]['end_timestamp'] * 1000){
+					row = '<tr class=\"highlight\">' + row + '</tr>';
+				}else{
+					row = tr(row);
+				}
+
+				addRow('#schedule'+server,row);
+			});
+			nextTime[server] = timeGrouped[server].values().next().value[0]['start_timestamp'];
 		}
-		function cdUpdateRepeat(){
-			cdUpdate();
-			setTimeout(cdUpdateRepeat, 60000);
-		}
-		populate();
-		if(window.localStorage.getItem('region') === null){
-			window.localStorage.setItem('region', 'JP');
-		}
-		if(window.localStorage.getItem('mode') === null){
-			window.localStorage.setItem('mode', 'group');
-		}
-		refreshSetting();
-		window.setTimeout(function(){
-			setTimeout(cdUpdate, 500);
-			setTimeout(cdUpdateRepeat, (60 - new Date().getSeconds()) * 1000);
-		}, 1000);
 	}
+	function populate() {
+		$.getJSON('./guerrilla_icon.json').done(function(data){
+			icon = data;
+		});
+		$.getJSON('https://storage.googleapis.com/mirubot/paddata/merged/guerrilla_data.json').done(loadSortedData);
+		//$.getJSON('./guerrilla_data.json').done(loadSortedData);
+	}
+	function cdUpdate() {
+		var now = new Date().getTime();
+		for (var server in timeGrouped){
+			if(nextTime[server] * 1000 - now < 0){
+				var next = 0;
+				for (let [k, v] of timeGrouped[server]) {
+					if(k * 1000 > now){
+						next = k;
+						break;
+					}
+				}
+				nextTime[server] = next;
+			}
+			if(activeTime[server] * 1000 - now < 0){
+				var next = 0;
+				for (let [k, v] of timeGrouped[server]) {
+					if(k * 1000 < now && v[0]['end_timestamp'] * 1000 > now){
+						next = k;
+						break;
+					}
+				}
+				activeTime[server] = next;
+			}
+			var clock;
+			var icon;
+			if(nextTime[server] > 0){
+				var distance = nextTime[server] * 1000 - now;
+				var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+				clock = hours + "h " + minutes + "m ";
+				icon = '<td>';
+				for (var dungeon of timeGrouped[server].get(nextTime[server])){
+					icon += '<span class="abcde">' + dungeon['group'] + '</span>' +getIcon(dungeon['dungeon_name']) + '</td><td>';
+				}
+				icon.substring(0, icon.length-4);
+			}else{
+				clock = 0;
+				icon = "None";
+			}
+			$('#nextTime'+server).html(clock);
+			$('#nextDungeon'+server).html(icon);
+			
+			if(activeTime[server] > 0){
+				var endtime = timeGrouped[server].get(activeTime[server])[0]['end_timestamp'];
+				var distance = endtime * 1000 - now;
+				var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+				clock = hours + "h " + minutes + "m";
+				icon = '<td>';
+				for (var dungeon of timeGrouped[server].get(activeTime[server])){
+					icon += '<span class="abcde">' + dungeon['group'] + '</span>' +getIcon(dungeon['dungeon_name']) + '</td><td>';
+				}
+				icon.substring(0, icon.length-4);
+			}else{
+				clock = "--h --m";;
+				icon = "None";
+			}
+			$('#activeTime'+server).html(clock);
+			$('#activeDungeon'+server).html(icon);
+		}
+	}
+	function cdUpdateRepeat(){
+		cdUpdate();
+		setTimeout(cdUpdateRepeat, 60000);
+	}
+	populate();
+	if(window.localStorage.getItem('region') === null){
+		window.localStorage.setItem('region', 'JP');
+	}
+	if(window.localStorage.getItem('mode') === null){
+		window.localStorage.setItem('mode', 'group');
+	}
+	refreshSetting();
+	window.setTimeout(function(){
+		setTimeout(cdUpdate, 500);
+		setTimeout(cdUpdateRepeat, (60 - new Date().getSeconds()) * 1000);
+	}, 1000);
+}
